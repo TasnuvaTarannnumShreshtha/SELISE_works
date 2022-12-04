af546dcf0b38a391d9586647c11f59f5d9d451a8
Shreshtha <tasnuva369@gmail.com>
2022-12-05 05:20:46 +0600

    Working.

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
 document.getElementById("btn").addEventListener("click", newTask);
 
+console.log("Working");
+
 function newTask() {
   const task = document.getElementById("task_input");
   const inputTask = task.value;
-
   var li = document.createElement("li");
   var t = document.createTextNode(inputTask);
   li.appendChild(t);
   document.getElementById("u_list").appendChild(li);
 }
-
-// btn.addEventListener("click", () => {
-//   const li = document.createElement("li");
-//   li.innerText = ul.value;
-//   ul.appendChild(li);
-
-// const para = document.getElementById("output1");
-// para.innerText = task;
-// console.log(task);
-// console.log(li);
-// });
